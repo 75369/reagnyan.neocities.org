9969077b56b2bf1d3977459be50671d1612839e4
75369 <43980717+75369@users.noreply.github.com>
2019-12-07 19:10:21 -0500

    Last updated 2019-10-25

---
+++ archive/2017-03-24/res/img.js
@@ -0,0 +1,3 @@
+// So I don't have to manually change the corner image on every page when I want to switch it around
+
+document.write('<div id="img-corner"><img src="../img/corner/dio.png"></div>');
